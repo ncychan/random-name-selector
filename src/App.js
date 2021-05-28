602e33a130d10b569a727d51314ddf3a238bc930
ncychan <46902611+ncychan@users.noreply.github.com>
2021-05-28 13:14:54 -0700

    created name selector (#1)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [nameBlock, setNameBlock] = useState('');
+  const [selectedName, setSelectedName] = useState();
+
+  const handleDrawName = () => {
+    const nameList = nameBlock.split(/[\r\n]+/)
+    const randomIndex = Math.floor(Math.random() * nameList.length)
+    setSelectedName(nameList[randomIndex])
+  }
+
+  const handleNameBlockChange = (evt) => {
+    const newValue = evt.target.value;
+    setNameBlock(newValue);
+  }
+
+  const handleClear = () => {
+    setNameBlock('')
+    setSelectedName(undefined)
+  }
+
+  // disable when nameBlock is undefined or empty string
+  const isDrawDisabled = !Boolean(nameBlock)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button disabled={isDrawDisabled} onClick={handleDrawName}>{Boolean(selectedName) ? 'Redraw Name' : 'Draw Name'}</button>
+      {(Boolean(nameBlock) || Boolean(selectedName)) && <button onClick={handleClear}>Clear</button>}
+
+      <textarea placeholder="Enter Names" value={nameBlock} onChange={handleNameBlockChange} />
+      { Boolean(selectedName) && <p>{selectedName}</p>}
     </div>
   );
 }
 
 export default App;
+
+
+/**
+ * false && * => false
+ * true && false => false
+ * true && true => true
+ *
+ * if selectedName is defined => Redraw Name, else => Draw Name
+ *
+ * condition ? true val : false val
+ *
+ * Boolean(selectedName) ? 'Redraw Name' : 'Draw Name'
+ *
+ * if nameBlock is defined || if selectedName is defined
+ * then Clear appear
+ *
+ * (Boolean(nameBlock) || Boolean(selectedName)) && <button>Clear</button>
+ *
+ * nameBlock === undefined || nameBlock === ''
+ * 
+ */
